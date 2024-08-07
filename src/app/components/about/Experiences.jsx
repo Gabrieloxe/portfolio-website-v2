b7f83893d90248880beec2eebfe0f30fd3788c91
Gabriel Ong <gabrielongxe@gmail.com>
2024-08-07 22:40:41 +0800

    make tech stack optional

--- src/app/components/about/Experiences.jsx
+++ src/app/components/about/Experiences.jsx
@@ -25,7 +25,7 @@ export const CompanyExperience = ({ company }) => {
           </h3>
           <p>{position.duration}</p>
           <ul className="mt-3 flex flex-col gap-3">
-            {position.responsibilities.map((responsibility, idx) => (
+            {position.responsibilities?.map((responsibility, idx) => (
               <li key={idx} className="text-base flex gap-2 text-secondary-50 mt-1">
                 <span className="text-primary-500 mt-1">
                   <ArrowTurnUpRightIcon className="h-5 w-5" />
@@ -37,7 +37,7 @@ export const CompanyExperience = ({ company }) => {
         </div>
       ))}
       <ul className="mt-2 mb-5 flex flex-wrap">
-        {company.techStack.map((tech, idx) => (
+        {company.techStack?.map((tech, idx) => (
           <li key={idx} className="mr-1.5 mt-2">
             <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-primary-300 ">
               {tech}
