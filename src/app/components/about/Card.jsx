69d637a224e57bd9a02a853b864bf7380a6439e7
Gabriel Ong <gabrielongxe@gmail.com>
2024-08-08 00:52:54 +0800

    update class to classname

--- src/app/components/about/Card.jsx
+++ src/app/components/about/Card.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-//// <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-primary-300 ">
 
 export const Card = ({ children }) => {
   return (
-    <div className="flex items-center rounded-xl leading-5 bg-secondary-100/10 py-6 px-4 mb-4">
+    <div className="flex items-center rounded-md leading-5 bg-sky-500/30 py-6 px-4 mb-4">
       {children}
     </div>
   );
--- src/app/components/projects/ProjectCard.jsx
+++ src/app/components/projects/ProjectCard.jsx
@@ -35,7 +35,7 @@ const ProjectCard = ({ imageUrl, title, description, githubUrl, previewUrl, stac
       <ul className="mt-2 mb-5 flex flex-wrap">
         {stack?.map((tech, idx) => (
           <li key={idx} className="mr-1.5 mt-2">
-            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-primary-300 ">
+            <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-primary-300 ">
               {tech}
             </div>
           </li>
